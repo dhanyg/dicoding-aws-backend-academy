89c192aff5db242d5f36eb1ae6c5d0ab53e9e90f
Ramadhan Yoga <ramayoga04@gmail.com>
2025-01-30 22:13:09 +0700

    latihan stream

---
+++ 07-stream/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const readableStream = fs.createReadStream('./input.txt', {
+    highWaterMark: 15
+});
+
+const writeableStream = fs.createWriteStream('./output.txt');
+
+readableStream.on('readable', () => {
+    try {
+        writeableStream.write(`${readableStream.read()}\n`)
+    }
+    catch (error) {
+        console.log(error);
+        return;
+    }
+});
+
+readableStream.on('end', () => {
+    writeableStream.end();
+});
